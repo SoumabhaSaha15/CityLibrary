6f9a928b8cbcab5d25199d125334d58ca062d506
S.Saha <soumabhasaha1509+github@gmail.com>
2026-01-19 22:03:44 +0530

    some changes done.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-// https://vite.dev/config/
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
   const targetUrl = env.VITE_CORS_URL;
